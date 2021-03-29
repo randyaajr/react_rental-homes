c22eff1bd410bf7dfa31f98794fa1ad15b7cab5b
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-03-29 16:38:53 -0500

    Created slider for houses

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styled, { css } from "styled-components/macro";
+import { Button } from "./Button";
 
 const HeroSection = styled.section`
   height: 100vh;
@@ -18,11 +19,37 @@ const HeroWrapper = styled.div`
   position: relative;
 `;
 
-const Hero = () => {
+const HeroSlide = styled.div``;
+const HeroSlider = styled.div``;
+const HeroImage = styled.div``;
+const HeroContent = styled.div``;
+
+const Hero = ({ slides }) => {
   return (
     <HeroSection>
       <HeroWrapper>
-        <h1>tesy</h1>
+        {slides.map((slide, index) => {
+          return (
+            <HeroSlide key={index}>
+              <HeroSlider>
+                <HeroImage />
+                <HeroContent>
+                  <h1>{slide.title}</h1>
+                  <p>{slide.price}</p>
+                  <Button
+                    to={slide.path}
+                    primary="true"
+                    css={`
+                      max-width: 160px;
+                    `}
+                  >
+                    {slide.label}
+                  </Button>
+                </HeroContent>
+              </HeroSlider>
+            </HeroSlide>
+          );
+        })}
       </HeroWrapper>
     </HeroSection>
   );
