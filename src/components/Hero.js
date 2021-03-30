e850b2d570cd9072fce6b3063065460c3cbf5e3c
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-03-29 20:56:14 -0500

    Slider logic created (bug issue).

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
 import styled, { css } from "styled-components/macro";
 import { Button } from "./Button";
+import { IoMdArrowRoundForward } from "react-icons/io";
+import { IoArrowForward, IoArrowBack } from "react-icons/io5";
 
 const HeroSection = styled.section`
   height: 100vh;
@@ -19,12 +21,151 @@ const HeroWrapper = styled.div`
   position: relative;
 `;
 
-const HeroSlide = styled.div``;
-const HeroSlider = styled.div``;
-const HeroImage = styled.div``;
-const HeroContent = styled.div``;
+const HeroSlide = styled.div`
+  z-index: 1;
+  width: 100%;
+  height: 100%;
+`;
+const HeroSlider = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  &::before {
+    content: "";
+    position: absolute;
+    z-index: 2;
+    width: 100%;
+    height: 100vh;
+    bottom: 0vh;
+    left: 0;
+    overflow: hidden;
+    opacity: 0.4;
+    background: linear-gradient(
+      0deg,
+      rgba(0, 0, 0, 0.6) 0%,
+      rgba(0, 0, 0, 0.6) 50%,
+      rgba(0, 0, 0, 1) 100%
+    );
+  }
+`;
+const HeroImage = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100vw;
+  height: 100vh;
+  object-fit: cover;
+`;
+const HeroContent = styled.div`
+  position: relative;
+  z-index: 10;
+  display: flex;
+  flex-direction: column;
+  max-width: 1600px;
+  width: calc(100% - 100px);
+  color: #fff;
+
+  h1 {
+    font-size: clamp(1rem, 8vw, 2rem);
+    font-weight: 400;
+    text-transform: uppercase;
+    text-shadow: 0px 0px 20px rgba(0, 0, 0, 0.4);
+    text-align: left;
+    margin-bottom: 0.8;
+  }
+
+  p {
+    margin-bottom: 1.2rem;
+    text-shadow: 0px 0px 20px rgba(0, 0, 0, 0.4);
+  }
+`;
+const Arrow = styled(IoMdArrowRoundForward)`
+  margin-left: 0.5rem;
+`;
+
+const SliderButtons = styled.div`
+  position: absolute;
+  bottom: 50px;
+  right: 50px;
+  display: flex;
+  z-index: 10;
+`;
+
+const arrowButtons = css`
+  width: 50px;
+  height: 50px;
+  color: #fff;
+  cursor: pointer;
+  background: #000d1a;
+  border-radius: 50px;
+  padding: 10px;
+  margin-right: 1rem;
+  user-select: none;
+  transitions: 0.3s;
+
+  &:hover {
+    background: #cd853f;
+    transform: scale(1.05);
+  }
+`;
+
+const PrevArrow = styled(IoArrowBack)`
+  ${arrowButtons}
+`;
+
+const NextArrow = styled(IoArrowForward)`
+  ${arrowButtons}
+`;
 
 const Hero = ({ slides }) => {
+  const [current, setCurrent] = useState(0);
+  const length = slides.length;
+  const timeout = useRef(null);
+
+  // useEffect(() => {
+  //   const nextSlide = () => {
+  //     setCurrent((current) => (current === length - 1 ? 0 : current + 1));
+  //   };
+
+  //   timeout.current = setTimeout(nextSlide, 1000);
+
+  //   return function () {
+  //     if (timeout.current) {
+  //       clearTimeout(timeout.current);
+  //     }
+  //   };
+  // }, [current, length]);
+
+  const nextSlide = () => {
+    if (timeout.current) {
+      clearTimeout(timeout.current);
+    }
+
+    setCurrent(current === length - 1 ? 0 : current + 1);
+
+    // console.log(current);
+  };
+
+  const prevSlide = () => {
+    if (timeout.current) {
+      clearTimeout(timeout.current);
+    }
+
+    setCurrent(current === 0 ? length - 1 : current - 1);
+
+    // console.log(current);
+  };
+
+  if (!Array.isArray(slides) || slides.length <= 0) {
+    return null;
+  }
+
   return (
     <HeroSection>
       <HeroWrapper>
@@ -32,10 +173,9 @@ const Hero = ({ slides }) => {
           return (
             <HeroSlide key={index}>
               <HeroSlider>
-                <HeroImage />
+                <HeroImage src={slide.image} alt={slide.alt} />
                 <HeroContent>
-                  <h1>{slide.title}</h1>
-                  <p>{slide.price}</p>
+                  <h1> {slide.title} </h1> <p> {slide.price} </p>
                   <Button
                     to={slide.path}
                     primary="true"
@@ -43,13 +183,16 @@ const Hero = ({ slides }) => {
                       max-width: 160px;
                     `}
                   >
-                    {slide.label}
+                    {slide.label} <Arrow />
                   </Button>
                 </HeroContent>
               </HeroSlider>
             </HeroSlide>
           );
         })}
+        <SliderButtons>
+          <PrevArrow onClick={prevSlide} /> <NextArrow onClick={nextSlide} />
+        </SliderButtons>
       </HeroWrapper>
     </HeroSection>
   );
