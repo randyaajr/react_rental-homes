69e2e4965d62a24e70e179357e6104dfa3a0105a
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-03-31 20:39:44 -0500

    Imported correct images to fit theme

--- src/data/InfoData.js
+++ src/data/InfoData.js
@@ -1,12 +1,12 @@
-import HouseTwelve from "../images/HouseTwelve.jpg";
-import HouseNine from "../images/HouseNine.jpg";
+import InteriorThree from "../images/InteriorThree.jpg";
+import PortraitThree from "../images/PortraitThree.jpg";
 
 export const InfoData = {
     heading: "Explore our beautiful homes!",
     ParagraphOne: "Our portfolio of more than 1,200 luxury villas encompasses the very best of the Europe with properties from Italy to Greece, Spain, and beyond. Whether you’re looking for Villa in Europe or a sprawling seaside estate for the whole family, we have it all.",
     ParagraphTwo: "The villas are traditional but luxurious giving one a sense of traditional lifestyle whilst simultaneously keeping you cool in the heart of design and modern comfort. Olympic length infinity pool, private cinema, Jacuzzi, SPA, tennis court and helipad embrace you with the feeling of having arrived somewhere very special.",
     buttonLabel: "View Homes",
-    image: HouseTwelve,
+    image: InteriorThree,
     reverse: false,
     delay: 100,
 };
@@ -16,7 +16,7 @@ export const InfoDataTwo = {
     ParagraphOne: "Open designs With abundant natural light, are descended from international architectural styles.",
     ParagraphTwo: "Many of our villa's come with social and versatile living space with wide sliding doors between circulation zones and an open plan living and dining room. A majority of the social spaces are designated to the middle/main floor with private areas distributed between the lower floor.",
     buttonLabel: "View Homes",
-    image: HouseNine,
+    image: PortraitThree,
     reverse: false,
     delay: 300,
 };
