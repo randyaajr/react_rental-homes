01edced70e8486bd79f0698e6103031b5aea5503
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-03-31 19:46:33 -0500

    Fixed issue causing bug

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import Dropdown from "./components/Dropdown";
 import Hero from "./components/Hero";
-import { InfoSection } from "./components/InfoSection";
+import InfoSection from "./components/InfoSection";
 import Navbar from "./components/Navbar";
 import { SliderData } from "./data/SliderData";
 import GlobalStyle from "./globalStyle";
