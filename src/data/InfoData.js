f803c8cbe256cddb04261e50a37859c8fa101818
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-03-31 20:44:26 -0500

    Corrected letter capitalization

--- src/data/InfoData.js
+++ src/data/InfoData.js
@@ -3,8 +3,8 @@ import PortraitThree from "../images/PortraitThree.jpg";
 
 export const InfoData = {
     heading: "Explore our beautiful homes!",
-    ParagraphOne: "Our portfolio of more than 1,200 luxury villas encompasses the very best of the Europe with properties from Italy to Greece, Spain, and beyond. Whether you’re looking for Villa in Europe or a sprawling seaside estate for the whole family, we have it all.",
-    ParagraphTwo: "The villas are traditional but luxurious giving one a sense of traditional lifestyle whilst simultaneously keeping you cool in the heart of design and modern comfort. Olympic length infinity pool, private cinema, Jacuzzi, SPA, tennis court and helipad embrace you with the feeling of having arrived somewhere very special.",
+    paragraphOne: "Our portfolio of more than 1,200 luxury villas encompasses the very best of the Europe with properties from Italy to Greece, Spain, and beyond. Whether you’re looking for Villa in Europe or a sprawling seaside estate for the whole family, we have it all.",
+    paragraphTwo: "The villas are traditional but luxurious giving one a sense of traditional lifestyle whilst simultaneously keeping you cool in the heart of design and modern comfort. Olympic length infinity pool, private cinema, Jacuzzi, SPA, tennis court and helipad embrace you with the feeling of having arrived somewhere very special.",
     buttonLabel: "View Homes",
     image: InteriorThree,
     reverse: false,
@@ -13,8 +13,8 @@ export const InfoData = {
 
 export const InfoDataTwo = {
     heading: "Modern Designs",
-    ParagraphOne: "Open designs With abundant natural light, are descended from international architectural styles.",
-    ParagraphTwo: "Many of our villa's come with social and versatile living space with wide sliding doors between circulation zones and an open plan living and dining room. A majority of the social spaces are designated to the middle/main floor with private areas distributed between the lower floor.",
+    paragraphOne: "Open designs With abundant natural light, are descended from international architectural styles.",
+    paragraphTwo: "Many of our villa's come with social and versatile living space with wide sliding doors between circulation zones and an open plan living and dining room. A majority of the social spaces are designated to the middle/main floor with private areas distributed between the lower floor.",
     buttonLabel: "View Homes",
     image: PortraitThree,
     reverse: false,
