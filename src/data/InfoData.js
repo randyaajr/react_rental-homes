064d118586cfd1bc4e54c1fc56366de1ac73043b
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-03-31 20:12:29 -0500

    Created data for the info section

---
+++ src/data/InfoData.js
@@ -0,0 +1,5 @@
+export const InfoData = {
+    heading: "Explore our beautiful homes!",
+    ParagraphOne: "Our portfolio of more than 1,200 luxury villas encompasses the very best of the Europe with properties from Italy to Greece, Spain, and beyond. Whether you’re looking for Villa in Europe or a sprawling seaside estate for the whole family, we have it all.",
+    ParagraphTwo: "The villas are traditional but luxurious giving one a sense of traditional lifestyle whilst simultaneously keeping you cool in the heart of design and modern comfort. Olympic length infinity pool, private cinema, Jacuzzi, SPA, tennis court and helipad embrace you with the feeling of having arrived somewhere very special.",
+};
