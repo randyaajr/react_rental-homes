7582e6e6bddd8fccee0c23129d792d76791465a4
randyaajr <67880000+randyaajr@users.noreply.github.com>
2021-03-31 19:54:40 -0500

    Created basic layout

--- src/components/InfoSection.js
+++ src/components/InfoSection.js
@@ -1,10 +1,27 @@
 import React from "react";
+import styled from "styled-components";
+import { Button } from "./Button";
+
+const Section = styled.section``;
+const Container = styled.div``;
+const ColumnLeft = styled.div``;
+const ColumnRight = styled.div``;
 
 const InfoSection = () => {
   return (
-    <div>
-      <h1>Info</h1>
-    </div>
+    <Section>
+      <Container>
+        <ColumnLeft>
+          <h1>Heading</h1>
+          <p>paragraph</p>
+          <p>paragraph</p>
+          <Button to="/homes">Homes</Button>
+        </ColumnLeft>
+        <ColumnRight>
+          <img src="#" alt="Luxury Villa" />
+        </ColumnRight>
+      </Container>
+    </Section>
   );
 };
 
